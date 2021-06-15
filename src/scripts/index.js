5594e3f968650e38e492d0aabca3d2a1aeba6c84
justinette2175 <jugagnepain75@gmail.com>
2021-06-15 17:14:27 -0400

    setup project

---
+++ src/scripts/index.js
@@ -0,0 +1,25 @@
+import "../styles/index.scss";
+
+import p5 from "p5";
+
+const CANVAS_WIDTH = 720;
+const CANVAS_HEIGHT = 400;
+
+const sketch = (s) => {
+  s.setup = () => {
+    s.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    s.background(220);
+  };
+
+  s.draw = () => {
+    s.background(220);
+    const mouse = s.createVector(s.mouseX, s.mouseY);
+    const center = s.createVector(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    mouse.sub(center);
+    mouse.setMag(300);
+    s.translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    s.line(0, 0, mouse.x, mouse.y);
+  };
+};
+
+new p5(sketch);
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+// Webpack uses this to work with directories
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const webpack = require("webpack");
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: ["@babel/polyfill", "./src/scripts/index.js"],
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      p5: "p5",
+    }),
+    new Dotenv(),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+    new ESLintPlugin(),
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8000,
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: "development",
+};
